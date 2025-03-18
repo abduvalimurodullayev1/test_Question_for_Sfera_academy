73719467f6cb98edf9d57d42ae05c97a4bb7a96e
Abduvali Murodullayev <125810900+abduvalimurodullayev1@users.noreply.github.com>
2025-03-18 12:06:55 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -3,6 +3,6 @@ from environs import Env
 env = Env()
 env.read_env()
 
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toek
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 IP = env.str("ip")  
